70febf1961b093ad6854edf1f072f525ed35f64c
g-bolsoni <giovanebolsoni2@gmail.com>
2021-11-16 20:35:30 -0300

    table responsive

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,13 +1,63 @@
-import React from 'react'
-import './style.scss'
-import Header from '../../components/Header';
+import React, {useState} from 'react'
+import './style.scss';
+import {toast} from 'react-toastify';
+import firebase from '../../services/firebaseConnection';
 
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import {FiUser} from 'react-icons/fi';
 
 
 export default function Customers() {
+    const [name_fantasia, setName_fantasia] = useState('');
+    const [cnpn_client, setCnpn_client] = useState('');
+    const [adress, setAdress] = useState('');
+
+    async function handleClient(e){
+        e.preventDefault()
+        if(name_fantasia !== '' && cnpn_client !== '' && adress !== ''){
+            await firebase.firestore().collection('custumers')
+            .add({
+                name_fantasia : name_fantasia,
+                cnpn_client: cnpn_client,
+                adress: adress
+            }).then(()=>{
+                setName_fantasia('');
+                setCnpn_client('');
+                setAdress('');
+                toast.info('Empresa cadastrada com sucesso!')
+            })
+            .catch((err)=>{
+                console.log(`Cadastro de usuário ${err}`);
+                toast.error('Algo deu errado, tente novamente')
+            })
+        }else{
+            toast.error('Preencha todos os campos!')
+        }
+    }
+
     return (
         <div className="customers_container">
             <Header/>
+            <div className="content">
+                <Title name="Clientes">
+                    <FiUser size={25} />
+                </Title>
+                <div className="container">
+                    <form className="form_profile" onSubmit={handleClient}>
+                        <label> Nome Fantasia:</label>
+                        <input placeholder="Nome da Empresa" type="text" value={name_fantasia} onChange={(e)=> setName_fantasia(e.target.value)} />
+
+                        <label>CNPJ:</label>
+                        <input placeholder="CNPJ da Empresa" type="text" value={cnpn_client} onChange={(e)=> setCnpn_client(e.target.value)} />
+
+                        <label> Endereço:</label>
+                        <input placeholder="Endereço da Empresa" type="text" value={adress} onChange={(e)=> setAdress(e.target.value)} />
+
+                        <button type="submit">  Cadastrar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,61 @@
-import { useContext } from 'react';
-import { UserContext } from '../../contexts/user';
+import './style.scss'
+import { useState } from 'react';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { Link } from 'react-router-dom';
+
+import {FiMessageSquare,FiPlus, FiSearch, FiEdit2} from 'react-icons/fi';
 export default function Dashboard() {
+    const [called, setcalled] = useState([])
     return (
-        <div>
+        <div className="dashboard_container">
             <Header/>
-            Página de Dashboard
+            <div className="content">
+                <Title name="Chamados" >
+                    <FiMessageSquare color="#000" size={25}/>
+                </Title>
+                {called.length !== 0 ? (
+                    <div className="container dashboard">
+                        <span> Nenhum chamado registrado </span>
+                        <Link to="/new" className="new"><FiPlus size={25} color="#fff"/>Novo Chamado</Link>
+                    </div>
+                ):(
+                    <div className="container active">
+                        <Link to="/new" className="new"><FiPlus size={25} color="#fff"/>Novo Chamado</Link>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Cadastrado em</th>
+                                    <th scope="col">#</th>
+                                </tr>
+
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td data-label="Cliente">Escola</td>
+                                    <td data-label="Assunto">Suporte</td>
+                                    <td data-label="Status">
+                                        <span className="badge" style={{background:'#5cb85c'}}>Em aberto</span>
+                                    </td>
+                                    <td data-label="Cadastrado">20/06/2021</td>
+                                    <td data-label="#">
+                                        <button className="action"style={{background:'#3583f6'}}>
+                                            <FiSearch color="#fff" size={17}/>
+                                        </button>
+                                        <button className="action"style={{background:'#f6a935'}}>
+                                            <FiEdit2 color="#fff" size={17}/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div> 
+                )}
+            </div>
         </div>
     )
 }
